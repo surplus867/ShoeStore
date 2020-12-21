26cb33964a8cc0c7ad9c60203f278af56e1734b3
Billy <yan.billy.20@gmail.com>
2020-12-20 19:48:24 -0800

    Edit ShareViewModel.kt Deleted Type Fragments

--- app/src/main/java/com/udacity/shoestore/ShareViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ShareViewModel.kt
@@ -1,4 +1,4 @@
-package viewModel
+package com.udacity.shoestore
 
 import android.util.Log
 import androidx.databinding.ObservableField
@@ -29,54 +29,54 @@ class ShareViewModel : ViewModel() {
     val shoeListNavigate: LiveData<String> get() = shoeListNavigationItem
     var shoeDetailNavigationItem = MutableLiveData<String>()
     val shoeDetailNavigate: LiveData<String> get() = shoeDetailNavigationItem
-    var shoeListBackNavigationItem = MutableLiveData<String>()
-    val shoeListBackNavigate: LiveData<String> get() = shoeListBackNavigationItem
+    var shoeListBackNavigationItem = MutableLiveData<ArrayList<Shoe>>()
+    val shoeListBackNavigate: LiveData<ArrayList<Shoe>> get() = shoeListBackNavigationItem
 
     // log-in Screen to Welcome Screen
     fun submitLogIn() {
         // if the username is empty, fire a toast event
-        if(username.get().orEmpty() != "Bilbo" && password.get().orEmpty() != "Bilbo"){
+        if (username.get().orEmpty() != "Bilbo" && password.get().orEmpty() != "Bilbo") {
             toastItem.value = " Please enter the correct email and password"
         // or else, fire a event to the WelcomeScreen
-        }else{
-            welcomeNavigationItem.value = ""
+         } else {
+            welcomeNavigationItem.postValue("")
         }
     }
     // Welcome Screen to Instruction Screen
     fun instructionNavigation() {
-        instructionNavigationItem.value = ""
+        instructionNavigationItem.postValue("")
     }
     // Instruction Screen to Shoe-List Screen
-    fun shoeListNavigation() {
-        shoeListNavigationItem.value = ""
-    }
+    fun shoeListNavigation() = shoeListNavigationItem.postValue("")
+
     // Shoe-List Screen to Shoe-Detail Screen
-    fun shoeDetailNavigation() {
-        shoeDetailNavigationItem.value = ""
+    fun shoeDetailNavigation() = shoeDetailNavigationItem.postValue("")
 
-    }
+    // navigate back to shoeList
+    fun backShoeList() = shoeListBackNavigationItem.postValue(shoeList.get())
 
-    //TODO name correctly ex.  submitShoe()
     fun submitShoe() {
-        Log.d("Bilbo", "ShoeName: ${shoename.get().orEmpty()}")
+        val localShoeList = ArrayList<Shoe>()
         val shoe = Shoe(shoename.get().orEmpty(),shoesize.get()?.toDouble() ?: 0.0,company.get().orEmpty(),description.get().orEmpty())
-        shoeList.get()?.add(shoe)
-        shoes.set("Name: ${shoename.get().orEmpty()} Company: ${company.get().orEmpty()}")
+        localShoeList.add(shoe)
+        shoeList.set(localShoeList)
         // navigate to list after save
-        shoeListBackNavigationItem.value = ""
-        //shoeDetailNavigationItem.value = ""
-    }
-
-    fun backShoeList() {
-        // navigate back to shoeList
-        shoeListBackNavigationItem.value = ""
-        //shoeDetailNavigationItem.value = ""
+       backShoeList()
     }
 
     fun showShoeList() {
-        Log.d("bilbo","Name: ${shoeList.get()?.first()?.name.orEmpty()} Company: ${shoeList.get()?.first()?.company.orEmpty()}")
+        // TODO grab shoeList and display the text either in text view or a recylcerview, *shoeRow
+        var exampleText = ""
+        shoeList.get()?.forEach {
+            // You need to iterate through the list with a for loop, you need to add the previous text to the new text and
+            // have a return carriage (/n) at the end of each line so that it put each shoe on a new line.
+            // Append the shoes items together. Append a string to string and return carriage at the end.
+            exampleText += "Shoe name ${it.name} Company: ${it.company} /n"
+        }
 
+        // sets the text to the list of shoes
+        shoes.set(exampleText)
+
+        //toastItem.value = shoeList.get()?.first()?.name.orEmpty()
     }
 }
-
-
